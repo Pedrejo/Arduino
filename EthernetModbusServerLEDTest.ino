36460c3a7874753f67bdd6d3dcd7403dcd8fb20e
Peter Fairbanks <101233005+Pedrejo@users.noreply.github.com>
2022-04-03 08:32:02 -0500

    Update EthernetModbusServerLEDTest.ino

--- EthernetModbusServerLEDTest.ino
+++ EthernetModbusServerLEDTest.ino
@@ -1,117 +1,67 @@
-/*
-  Ethernet Modbus TCP Server LED
-
-  This sketch creates a Modbus TCP Server with a simulated coil.
-  The value of the simulated coil is set on the LED
-
-  Circuit:
-   - Any Arduino MKR Board
-   - MKR ETH Shield
-
-  created 16 July 2018
-  by Sandeep Mistry
-*/
-#include <P1AM.h>
 #include <SPI.h>
 #include <Ethernet.h>
-
-#include <ArduinoRS485.h> // ArduinoModbus depends on the ArduinoRS485 library
+#include <P1AM.h>
+#include <ArduinoRS485.h>
 #include <ArduinoModbus.h>
 
-// Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
-// The IP address will be dependent on your local network:
 byte mac[] = {
   0x60, 0x52, 0xD0, 0x06, 0xEF, 0xD7
 };
 IPAddress ip(192, 168, 1, 121);
-
 EthernetServer ethServer(502);
-
 ModbusTCPServer modbusTCPServer;
-
-const int ledPin = LED_BUILTIN;
+int coilValue = 0;
 
 void setup() {
-  // You can use Ethernet.init(pin) to configure the CS pin
-  //Ethernet.init(10);  // Most Arduino shields
-  //Ethernet.init(5);   // MKR ETH shield
-  //Ethernet.init(0);   // Teensy 2.0
-  //Ethernet.init(20);  // Teensy++ 2.0
-  //Ethernet.init(15);  // ESP8266 with Adafruit Featherwing Ethernet
-  //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
-
-  // Open serial communications and wait for port to open:
-  Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  Serial.println("Ethernet Modbus TCP Example");
-
-  // start the Ethernet connection and the server:
-  while (!P1.init()){}; //Wait for P1 to sign on
+  Serial.begin(9600);
   Ethernet.begin(mac, ip);
-
-  // Check for Ethernet hardware present
-  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-    while (true) {
-      delay(1); // do nothing, no point running without Ethernet hardware
-    }
-  }
-  if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println("Ethernet cable is not connected.");
-  }
-
-  // start the server
   ethServer.begin();
-  
-  // start the Modbus TCP server
-  if (!modbusTCPServer.begin()) {
-    Serial.println("Failed to start Modbus TCP Server!");
-    while (1);
+  modbusTCPServer.begin();
+  while (!P1.init()){
+    ;
   }
-
-  // configure the LED
-  //   pinMode(ledPin, OUTPUT);
-  //   digitalWrite(ledPin, LOW);
-
-  // configure a single coil at address 0x00
   modbusTCPServer.configureCoils(0x00, 1);
 }
 
 void loop() {
-  // listen for incoming clients
-  EthernetClient client = ethServer.available();
-  
-  if (client) {
-    // a new client connected
-    Serial.println("new client");
 
+  EthernetClient client = ethServer.available();
+  if (client) {
     // let the Modbus TCP accept the connection 
     modbusTCPServer.accept(client);
 
     while (client.connected()) {
       // poll for Modbus TCP requests, while client connected
       modbusTCPServer.poll();
-
-      // update the LED
-      updateLED();
+      //delay(50);
+      coilValue = modbusTCPServer.coilRead(0x00);
     }
-
-    Serial.println("client disconnected");
+    updateRelays();
   }
 }
 
-void updateLED() {
-  // read the current value of the coil
-  int coilValue = modbusTCPServer.coilRead(0x00);
-  Serial.println(coilValue);
+void updateRelays() {
+  
   if (coilValue) {
-    // coil value set, turn LED on
-    P1.writeDiscrete(HIGH,1,2);  //Turn slot 1 channel 2 on
+      P1.writeDiscrete(0x01,1);
+      delay(80);
+      P1.writeDiscrete(0x04,1);
+      delay(80);
+      P1.writeDiscrete(0x10,1);
+      delay(80);
+      P1.writeDiscrete(0x40,1);
+      delay(80);
+      P1.writeDiscrete(0x00,1);
+
   } else {
-    // coil value clear, turn LED off
-    P1.writeDiscrete(LOW,1,2);  //Turn slot 1 channel 2 on
+      P1.writeDiscrete(0x02,1);
+      delay(80);
+      P1.writeDiscrete(0x08,1);
+      delay(80);
+      P1.writeDiscrete(0x20,1);
+      delay(80);
+      P1.writeDiscrete(0x80,1);
+      delay(80);
+      P1.writeDiscrete(0x00,1);
   }
 }
