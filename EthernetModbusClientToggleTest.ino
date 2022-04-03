885c1dd2f0e45d6624664972cc40a7fa08f040a6
Peter Fairbanks <101233005+Pedrejo@users.noreply.github.com>
2022-04-03 08:30:03 -0500

    Update EthernetModbusClientToggleTest.ino

--- EthernetModbusClientToggleTest.ino
+++ EthernetModbusClientToggleTest.ino
@@ -1,81 +1,76 @@
-/*
-  Ethernet Modbus TCP Client Toggle
-
-  This sketch toggles the coil of a Modbus TCP server connected
-  on and off every second.
-
-  Circuit:
-   - Any Arduino MKR Board
-   - MKR ETH Shield
-
-  created 16 July 2018
-  by Sandeep Mistry
-*/
-#include <P1AM.h>
 #include <SPI.h>
 #include <Ethernet.h>
-
-#include <ArduinoRS485.h> // ArduinoModbus depends on the ArduinoRS485 library
+#include <P1AM.h>
+#include <ArduinoRS485.h>
 #include <ArduinoModbus.h>
 
-// Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
-// The IP address will be dependent on your local network:
 byte mac[] = {
   0x60, 0x52, 0xD0, 0x06, 0xEF, 0xE5
 };
 IPAddress ip(192, 168, 1, 120);
-
 EthernetClient ethClient;
 ModbusTCPClient modbusTCPClient(ethClient);
+IPAddress server(192, 168, 1, 121);
 
-IPAddress server(192, 168, 1, 121); // update with the IP Address of your Modbus server
+int simInput = 0;
+int pinState = 0;
+int lastPinState = 0;
 
 void setup() {
+  Serial.begin(9600);
   Ethernet.begin(mac, ip);
-  modbusTCPClient.setTimeout(500);//Adjust Response Timeout from 30 seconds to 500 ms.
-  //Initialize serial and wait for port to open:
-  Serial.begin(115200);
-  while (!Serial) {};// wait for serial port to connect.
-  Serial.println("Modbus TCP Client Simple Example");
-  Serial.print("P1AM-ETH at IP:");
-  Serial.println(ip);
-
-  // start the Ethernet connection and the server:
-  
-
-
+  while (!P1.init()){
+    ;
+  }
 }
 
 void loop() {
-  if (!modbusTCPClient.connected()) {
-    // client not connected, start the Modbus TCP client
-    Serial.println("Attempting to connect to Modbus TCP server");
+
+  pinState = P1.readDiscrete(2,1);
+  if (pinState != lastPinState) {
+    modbusTCPClient.begin(server, 502);
     
-    if (!modbusTCPClient.begin(server, 502)) {
-      Serial.println("Modbus TCP Client failed to connect!");
+    if (pinState) {
+      relaysOn();
+      modbusTCPClient.coilWrite(0x00, 0x01);
+      modbusTCPClient.stop();
+     // relaysOn();
     } else {
-      Serial.println("Modbus TCP Client connected");
-    }
-  } else {
-    // client connected
-
-    // write the value of 0x01, to the coil at address 0x00
-    if (!modbusTCPClient.coilWrite(0x00, 0x01)) {
-      Serial.print("Failed to write coil! ");
-      Serial.println(modbusTCPClient.lastError());
-    }
-
-    // wait for 1 second
-    delay(10000);
-
-    // write the value of 0x00, to the coil at address 0x00
-    if (!modbusTCPClient.coilWrite(0x00, 0x00)) {
-      Serial.print("Failed to write coil! ");
-      Serial.println(modbusTCPClient.lastError());
-    }
-
-    // wait for 1 second
-    delay(10000);
+      relaysOff();
+      modbusTCPClient.coilWrite(0x00, 0x00);
+      modbusTCPClient.stop();
+    //  relaysOff();
+    }   
   }
+
+  lastPinState = pinState;
+
+}
+
+void relaysOn() {
+      
+  P1.writeDiscrete(0x01,1);
+  delay(80);
+  P1.writeDiscrete(0x04,1);
+  delay(80);
+  P1.writeDiscrete(0x10,1);
+  delay(80);
+  P1.writeDiscrete(0x40,1);
+  delay(80);
+  P1.writeDiscrete(0x00,1);
+   
+}
+
+void relaysOff() {
+
+  P1.writeDiscrete(0x02,1);
+  delay(80);
+  P1.writeDiscrete(0x08,1);
+  delay(80);
+  P1.writeDiscrete(0x20,1);
+  delay(80);
+  P1.writeDiscrete(0x80,1);
+  delay(80);
+  P1.writeDiscrete(0x00,1);
+
 }
